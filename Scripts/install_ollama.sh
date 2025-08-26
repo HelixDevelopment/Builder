cb11df56e0657fe164a6cacfc1c0207a74abbf18
Milos Vasic <i@mvasic.ru>
2025-08-26 17:24:11 +0300

    TODO added.

--- Scripts/install_ollama.sh
+++ Scripts/install_ollama.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# TODO: Make sure it binds to 0.0.0.0 and open Firewall ports
+
 if curl -fsSL https://ollama.ai/install.sh | sh && \
     sudo systemctl start ollama && \
     sudo systemctl enable ollama; then
