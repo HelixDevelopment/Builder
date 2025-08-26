699465f54de380a7e921df2ba35286909275ac65
Milos Vasic <i@mvasic.ru>
2025-08-26 17:16:00 +0300

    Port fix.

--- Scripts/install_ollama_webui.sh
+++ Scripts/install_ollama_webui.sh
@@ -25,7 +25,7 @@ else
     PORT="$1"
 fi
 
-if docker run -d -p "$PORT":"$PORT" --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui ghcr.io/open-webui/open-webui:main; then
+if docker run -d -p "$PORT":8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui ghcr.io/open-webui/open-webui:main; then
 
     echo "Installation completed, running at port: $PORT"
 
