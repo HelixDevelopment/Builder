340d6a3d83f83c47291b93294b139df3b6cc29b4
Milos Vasic <i@mvasic.ru>
2025-08-26 16:36:19 +0300

    Ollama webui installer.

---
+++ Scripts/install_ollama_webui.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if ! which docker; then
+
+    echo "ERROR: Please install Docker"
+    exit 1
+fi
+
+if [ -z "$1" ]; then
+
+    PORT="8085"
+
+else
+
+    PORT="$1"
+fi
+
+if docker run -d -p "$PORT":"$PORT" --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui ghcr.io/open-webui/open-webui:main; then
+
+    echo "Installation completed, running at port: $PORT"
+
+else
+
+    echo "ERROR: Installation failed"
+    exit 1
+fi
